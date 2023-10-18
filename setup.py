5ced635c57b312a1411ad775468a22a9c94083a5
Vikentii Smetanin <smetaninvikenti@yandex.ru>
2023-10-18 08:39:03 +0300

    update

--- setup.py
+++ setup.py
@@ -8,23 +8,18 @@ def readme():
 
 setup(
   name='TelegramBotsCallbackData',
-  version='16.10.2023',
+  version='2023.10.16',
   author='Wintreist',
   author_email='wintreist1811@gmail.com',
   description='This is a library for storing a lot of information in callback data',
   long_description=readme(),
   long_description_content_type='text/markdown',
-  url='',
+  url='https://github.com/Wintreist/TelegramBotsCallbackData',
   packages=find_packages(),
-  install_requires=['requests>=2.25.1'],
-  classifiers=[
-    'Programming Language :: Python :: 3.11',
-    'License :: OSI Approved :: MIT License',
-    'Operating System :: OS Independent'
-  ],
-  keywords='files speedfiles ',
+  install_requires=[],
+  keywords='Telegram Callback',
   project_urls={
-    'GitHub': 'your_github'
+    'GitHub': 'https://github.com/Wintreist'
   },
   python_requires='>=3.6'
 )
